5f6c165e26082bd1305a31ef141e31063f999c27
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2019-07-24 16:32:12 +0900

    powerful Link high order component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const Index = () => (
   <div>
     <Link href="/about">
-      <a>About Page</a>
+      <button>Go to About Page</button>
     </Link>
     <p>Hello Next.js</p>
   </div>
