b99e052ab32ec13eb5718b5948348c286bfc7435
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2019-07-25 16:54:27 +0900

    add fetch API function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,34 @@
 import Layout from "../components/MyLayout";
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-const PostLink = (props: any) => (
-  <li>
-     {/* href={`/post?title=${props.title}`} */}
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
-      <a>{props.id}</a>
-    </Link>
 
-  </li>
-);
-
-const Index = () => (
+const Index = (props: any) => (
   <Layout>
-    <h1>My Blog</h1>
+    <h1>Batman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs"/>
-      <PostLink id="Learn-nextjs"/>
-      <PostLink id="deploy-nextjs"/>
-
+      {
+        props.shows.map((show: any) => (
+          <li key={show.id}>
+            <Link href="/p/[id]" as={`/p/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        ))
+      }
     </ul>
   </Layout>
 );
+
+Index.getInitialProps = async function () {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(data.length);
+
+  return {
+    shows: data.map((entry: any) => entry.show)
+  };
+};
 
 export default Index;
--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,15 +1,22 @@
-import { useRouter } from 'next/router';
 import Layout from '../../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
 
-const Post = () => {
-  const router = useRouter();
+const Post = (props: any) => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium} />
+  </Layout>
+);
 
-  return (
-    <Layout>
-      <h1>{router.query.id}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  )
-}
+Post.getInitialProps = async function(context: any) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`${show.name}`);
+
+  return { show };
+};
 
 export default Post;
